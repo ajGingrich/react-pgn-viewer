83166bdc087669aeb246491953558a42fc8573c8
Andrew Gingrich <ajgingrich@yahoo.com>
2018-05-01 18:45:46 -0600

    remove event listener props from chess board

--- src/index.js
+++ src/index.js
@@ -29,31 +29,12 @@ class PgnViewer extends React.Component {
           isDraggable={true} // Can the pieces be dragged? Default: true
           orientation="b" // ['w', 'b'] Default: 'w'
           // pieceTheme="uscf" // ['alpha', 'uscf', 'wikipedia'] Default: 'wikipedia'
-          showCoordinates={false} // Default: true
           style={{
             border: '2px solid lightgrey',
           }}
           whiteSquareColour="aliceblue" // Default: '#f0d9b5'
           width={400} // String ('100%', of container) | number (px). If expressed as a percentage,
           // the board will resize with its container. Default: 400
-
-          // Events: similar to chessboard.js events. Additional onResize, onSquareClick events.
-          onChange={(oldPos, newPos) => console.log('onChange fired', oldPos, newPos)}
-          onDragMove={(algebraic, fromSquare, piece, fen, orientation) =>
-            console.log('onDragMove fired', algebraic, fromSquare, piece, fen, orientation)}
-          onDragStart={(square, piece, fen, orientation) =>
-            console.log('onDragStart fired', square, piece, fen, orientation)}
-          onDrop={(square, toSquare, piece, newPosition, fen, orientation) =>
-            console.log('onDrop fired', square, toSquare, piece, newPosition, fen, orientation)}
-          onMouseOutSquare={(algebraic, piece, fen, orientation) =>
-            console.log('onMouseOutSquare fired', algebraic, piece, fen, orientation)}
-          onMouseOverSquare={(algebraic, piece, fen, orientation) =>
-            console.log('onMouseOverSquare fired', algebraic, piece, fen, orientation)}
-          onMoveEnd={(oldPos, newPos) => console.log('onMoveEnd fired', oldPos, newPos)}
-          onResize={(oldWidth, newWidth) => console.log('onResize fired', oldWidth, newWidth)}
-          onSnapbackEnd={(piece, square, fen, orientation) =>
-            console.log('onSnapbackEnd fired', piece, square, fen, orientation)}
-          onSquareClick={(isRightClick, square, piece, fen, orientation) => console.log(isRightClick, square, piece, fen, orientation)}
         />
       </div>
     )
