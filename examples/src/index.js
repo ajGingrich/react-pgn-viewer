f11482cbcc3ae0fdf691af462928ec5ade2d4ba3
Andrew Gingrich <ajgingrich@yahoo.com>
2018-05-10 12:19:21 -0600

    add first sample react highlight for developing

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,6 +1,7 @@
 import { AppContainer } from 'react-hot-loader';
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import '../../node_modules/highlight.js/styles/darcula.css';
 import Root from './Root'
 
 const render = Component =>
