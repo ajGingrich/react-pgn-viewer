60656d6db4ec9d99238e4f4efdb5aa4fa8e5936f
Andrew Gingrich <ajgingrich@yahoo.com>
2018-05-21 17:55:34 -0600

    publish first version

--- gulpfile.js
+++ gulpfile.js
@@ -2,34 +2,39 @@ var gulp = require("gulp")
 var rollup = require('rollup-stream')
 var source = require('vinyl-source-stream')
 var webserver = require("gulp-webserver")
+var del = require("del")
+var env = process.env.NODE_ENV
+var isProduction = env === 'production'
+
+gulp.task("clean", function() {
+  return del('dist/**', {force:true})
+})
 
 gulp.task("copy", function() {
   return gulp.src("examples/*.html")
     .pipe(gulp.dest("dist"))
 })
 
-///do something with environment here so i can use one file??
-
 gulp.task("rollup", function() {
   return rollup({
-    input: "examples/index.js",
-    format: "iife",
+    input: isProduction ? "src/index.js" : "examples/index.js",
+    format: isProduction ? "cjs" : "iife",
     name: 'pgn',
     plugins: [
-      require("rollup-plugin-replace")({
-        'process.env.NODE_ENV': JSON.stringify('production'),
+      require("rollup-plugin-babel")({
+        exclude: 'node_modules/**',
+        // plugins: ['external-helpers'],
+        externalHelpers: false
       }),
       require("rollup-plugin-node-resolve")({
         jsnext: true,
         main: true,
         browser: true
       }),
-      require("rollup-plugin-babel")({
-        exclude: 'node_modules/**'
-      }),
       require("rollup-plugin-commonjs")({
         include: 'node_modules/**',
-        exclude: 'node_modules/chess.js', // doesn't work with es6
+        exclude: 'node_modules/react-chessboardjs/index.js',
+         /// exclude react?? only in external
         namedExports: {
           'node_modules/react/index.js': [
             'Component',
@@ -38,12 +43,12 @@ gulp.task("rollup", function() {
             'Children',
             'createElement',
           ]
-          // 'node_modules/chess.js/chess.js': [
-          //   'Chess',
-          // ],
         },
-      })
-      // there is a bug with 9.1.3 see https://github.com/dherges/ng-packagr/issues/657
+      }), // bug with 9.1.3 see https://github.com/dherges/ng-packagr/issues/657
+      require("rollup-plugin-replace")({
+        'process.env.NODE_ENV': JSON.stringify(env),
+      }),
+      require("rollup-plugin-uglify") //it's not being uglfied
     ],
   }).pipe(source("bundle.js"))
     .pipe(gulp.dest("dist"));
@@ -61,4 +66,6 @@ gulp.task("watch", function() {
   gulp.watch("src/**/*.js", ["rollup"]) /// also watch examples here?!
 });
 
-gulp.task("default", ["copy", "rollup", "server", "watch"]);
+gulp.task("dev", ["clean", "copy", "rollup", "server", "watch"]);
+
+gulp.task("prod", ["clean", "rollup"]);
