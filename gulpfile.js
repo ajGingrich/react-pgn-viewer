976a96185f5bf6e6278d1e78d195d985dae810b1
Andrew Gingrich <ajgingrich@yahoo.com>
2018-06-13 18:28:25 -0600

    add analyzer for bundle

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,8 @@ var webserver = require("gulp-webserver")
 var del = require("del")
 var env = process.env.NODE_ENV
 var isProduction = env === 'production'
-// bug with rollup-common-js 9.1.3 see https://github.com/dherges/ng-packagr/issues/657)
+var analyzerOptions = { limit: 15, filter: null }
+// bug with rollup-common-js 9.1.3 see https://github.com/dherges/ng-packagr/issues/657
 
 gulp.task("clean", function() {
   return del('dist/**', {force:true})
